8d6278e6cc5ccf55d416e01e339843d929a68c27
juaccardenasgom <juaccardenasgom@unal.edu.co>
2018-09-07 12:44:56 -0500

    setup

--- manage.py
+++ manage.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import raven
-
-RAVEN_CONFIG = {
-    'dsn': 'https://9189cccf8cbd4829bf893cc021a5157a:457bc29f5129417287dcb2840ee3d4d3@sentry.io/1276449',
-    # If you are using git, you can also automatically configure the
-    # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.abspath(os.curdir)),
-}
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
