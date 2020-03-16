8b92b7441960a4cf5927fbd01c407a12458ae3f5
Matt Harris <mjharris2407@gmail.com>
2020-03-16 17:39:08 -0400

    development configs and tools

--- settings.py
+++ settings.py
@@ -1,30 +1,25 @@
 from os import environ
 
+
 # ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 # ADMIN_PASSWORD = 'GoBoilers2018!'
 # OTREE_PRODUCTION Consider '', None, and '0' to be empty/false
+# SENTRY_DSN = environ.get('SENTRY_DSN')
+# AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
 
-SENTRY_DSN = environ.get('SENTRY_DSN')
-AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
+DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'USD'
 USE_POINTS = True
 POINTS_CUSTOM_NAME = 'tokens'
-DEBUG = (environ.get('OTREE_PRODUCTION') in {None, '', '0'})
 AUTH_LEVEL = 'DEMO'
 ADMIN_USERNAME = 'admin'
 ADMIN_PASSWORD = 'admin'
 SECRET_KEY = '68j-5$9@d=m0_*pb0n=_3u6%33fng(738yww&w^ttk136n(4vz'
-INSTALLED_APPS = ['otree']
-
-DEMO_PAGE_INTRO_HTML = """
-In order to test
-</br>
-USER: admin</br>
-PASS: admin
-</br></br>
-"""
+INSTALLED_APPS = [
+    'otree',
+]
 
 ROOMS = [{
     'name': 'test_in_progress',
@@ -64,12 +59,33 @@ SESSION_CONFIGS = [
         'name': 'main_app',
         'display_name': "Energy Conservation Game",
         'num_demo_participants': 1,
-        'app_sequence': ['main_app']
+        'app_sequence': ['onboarding_app' ,'energy_game', 'post_game']
     },
     {
         'name': 'onboarding',
-        'display_name': "Intro and Onboarding",
+        'display_name': "Introduction, Practice Round, and Quiz",
         'num_demo_participants': 1,
         'app_sequence': ['onboarding_app'],
+    },
+    {
+        'name': 'energy_game',
+        'display_name': "Game Only",
+        'num_demo_participants': 1,
+        'app_sequence': ['energy_game'],
+    },
+    {
+        'name': 'post_game',
+        'display_name': "Post-Game Survey Only",
+        'num_demo_participants': 1,
+        'app_sequence': ['post_game'],
     }
 ]
+
+
+DEMO_PAGE_INTRO_HTML = """
+In order to test
+</br>
+USER: admin</br>
+PASS: admin
+</br></br>
+"""
